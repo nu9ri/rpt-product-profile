0bc777536ec43cbe5af380a2da72f4678b964624
nu9ri <rmffheldk3@gmail.com>
2026-07-31 12:39:29 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "rpt-profile-mobile-v2";
+const CACHE_NAME = "rpt-profile-mobile-v3";
 const APP_FILES = ["./", "./index.html", "./manifest.json", "./icon.svg"];
 
 self.addEventListener("install", event => {
